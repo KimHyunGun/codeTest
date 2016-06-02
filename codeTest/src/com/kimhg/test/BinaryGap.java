5603b5464724db934a8253ac3784971c9d7faa17
KimHyunGun <kimhg787@gmail.com>
2016-06-02 11:43:32 +0900

    first commit

---
+++ codeTest/src/com/kimhg/test/BinaryGap.java
@@ -0,0 +1,32 @@
+package com.kimhg.test;
+
+public class BinaryGap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tmpNum = 9;
+		String binString = Integer.toBinaryString(tmpNum);
+		binString = "11111001000000100";
+		
+		int ret = 0;
+		for(int i = 0; i<binString.length(); i++){
+			if(binString.charAt(i) == '1'){
+				int zeroCnt = 0;
+				
+				for(int j = i+1; j<binString.length(); j++){
+					if(binString.charAt(j) == '0'){
+						zeroCnt++;
+					}else{
+						if(ret<zeroCnt)
+							ret = zeroCnt;
+						break;
+					}
+				}
+				
+			}
+		}
+		
+		System.out.println("결과 : "+ret);
+	}
+
+}
