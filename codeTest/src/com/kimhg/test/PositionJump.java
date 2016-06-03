b072da2ef8ef4f23a1ed3419fba386fd50419dcf
KimHyunGun <kimhg787@gmail.com>
2016-06-03 14:27:44 +0900

    1

--- codeTest/src/com/kimhg/test/PositionJump.java
+++ codeTest/src/com/kimhg/test/PositionJump.java
@@ -8,30 +8,16 @@ public class PositionJump {
 		// TODO Auto-generated method stub
 		PositionJump pj = new PositionJump();
 		
-		Random ran = new Random();
-		int X = ran.nextInt(10000)+1;
-		int Y = ran.nextInt(1000000)+1;
-		int D = ran.nextInt(1000000)+1;
-		D = 2;
-		for(;Y<X;){
-			Y = ran.nextInt(1000000)+1;
-		}
-		//System.out.println(X+", "+Y+", "+D);
 		int ret = pj.solution(1, 1000000000, 2);
-		//int ret = 0;
 		System.out.println("결과 : "+ret);
 	}
 
 	public int solution(int X, int Y, int D){
-		/*
-		X = 5;
-		Y = 18579721;
-		D = 2;
-		*/
 		int ret = 0;
-		int distance = Y - X;
 
+		/*
 	    //not a valid distance to jump
+		int distance = Y - X;
 	    if(distance <= 0){
 	        ret = 0;
 	    }else if(distance % D > 0){
@@ -39,7 +25,7 @@ public class PositionJump {
 	    }else{
 	        ret = distance/D;
 	    }
-		
+		*/
 		
 	    ret = (int) Math.ceil( (Y-X) / (double)D );
 	    
---
+++ codeTest/src/com/kimhg/test/TapeEquilibrium.java
@@ -0,0 +1,41 @@
+package com.kimhg.test;
+
+public class TapeEquilibrium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TapeEquilibrium te = new TapeEquilibrium();
+		
+		int[] arr = {3, 1, 2, 4, 3};
+		int ret = te.solution(arr);
+		System.out.println("결과 : "+ret);
+	}
+
+    public int solution(int[] A) {
+		// write your code in Java SE 8
+		int ret = 0;
+		
+		int totalSum = 0;
+		for(int i = 0; i < A.length; i++){
+			totalSum += A[i];
+		}
+		int firstSum = 0;
+		int secondSum = 0;
+		for(int j = 1; j < A.length; j++){
+			firstSum += A[j-1];
+			secondSum = totalSum - firstSum; 
+			    
+			System.out.println(j+", |"+firstSum+" - "+secondSum+"|"+ Math.abs(firstSum-secondSum)+", "+totalSum);
+			int diffSum = Math.abs(firstSum-secondSum);
+			if(j == 1 || ret >= diffSum){
+				ret = diffSum;
+				System.out.println(ret+" , "+diffSum);
+		    }
+		}
+		
+		return ret;
+		        
+	}
+       
+}
