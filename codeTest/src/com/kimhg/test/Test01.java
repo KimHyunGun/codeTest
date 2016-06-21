7619fc38f5e801133da259a176789c3e639ddeaa
kimhg <kimhg@192.168.35.43>
2016-06-21 20:51:34 +0900

    1

---
+++ codeTest/src/com/kimhg/test/Test01.java
@@ -0,0 +1,11 @@
+package com.kimhg.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String tmp1 = "hi i i I!";
+		System.out.println(tmp1.replaceAll("i", "T"));
+	}
+
+}
